26ccd46cfc9337a8929b638467af455002bb771e
Prashant773 <pkr2246@gmail.com>
2026-02-04 15:36:48 +0530

    first commit

---
+++ src/main/java/com/example/test_psa/TestPsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPsaApplication {
+int x = 100;
+	public static void main(String[] args) {
+		SpringApplication.run(TestPsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_psa/TestPsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_psa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
